4da0fef86f70920736f2e39d1001c4e046446592
skilltechs514 <skilltechs.net@gmail.com>
2023-07-25 00:25:48 +0500

    Fixed icons

--- contactUs.php
+++ contactUs.php
@@ -8,7 +8,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   $to = 'yousafarslan09@gmail.com';
   $subject = 'New email from website';
   $body = "Name: $name\nEmail: $email\n\n$message";
-
+  ini_set("SMTP", "https://agreeable-cliff-0bd37e810.3.azurestaticapps.net/");
+  ini_set("smtp_port", 25);
   // Send email
   if (mail($to, $subject, $body)) {
     echo 'Email sent successfully.';
