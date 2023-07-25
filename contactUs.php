f509075f72b4c00dcaf8161122ccab160e13bba9
skilltechs514 <skilltechs.net@gmail.com>
2023-07-25 16:23:13 +0500

    Fixed icons

--- contactUs.php
+++ contactUs.php
@@ -1,59 +1,33 @@
 <?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  $name = $_POST['name'];
-  $email = $_POST['email'];
-  $number = $_POST['number'];
-  $message = $_POST['message'];
-  $headers = "From: info@arclighttech.net\r\n";
-  $headers .= "Reply-To: yousafarslan09@gmail.com\r\n";
-  $headers .= "Return-Path: yousafarslan09@gmail.com\r\n"; // Make sure to use a valid email address here.
-  $headers .= "Content-Type: text/plain; charset=utf-8\r\n";
-  $headers .= "X-Mailer: PHP/" . phpversion();
-  $to = 'yousafarslan09@gmail.com';
-  $subject = 'New email from website';
-  $body = "Name: $name\nEmail: $email\n\n$message";
-  ini_set("SMTP", "arclighttech.net");
-  ini_set("smtp_port","25");
-  ini_set("info@arclighttech.net", "yousafarslan09@gmail.com");
-  ini_set("smtp_ssl", "tls");
-  // Send e$mail
-  if (mail($to, $subject, $body, $headers)) {
-    echo 'Email sent successfully.';
-  } else {
-    echo 'An error occurred. Please try again later.';
-  }
-}
-// require 'PHPMailer\src\PHPMailer.php';
-// require 'PHPMailer\src\Exception.php';
-// require 'PHPMailer\src\SMTP.php';
-// $mail = new PHPMailer\PHPMailer\PHPMailer();
-// $mail->isSMTP();
-// $mail->Host = 'smtp.gmail.com';  // Replace with your SMTP server
-// $mail->Port = 587;               // Replace with your SMTP port
-// $mail->SMTPAuth = true;
-// $mail->Username = 'yousafarslan09@gmail.com';   // Replace with your SMTP username (email address)
-// $mail->Password = 'Nathy9176';           // Replace with your SMTP password
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-// $mail->SMTPSecure = 'tls';
-// $mail->SMTPDebug = PHPMailer\PHPMailer\SMTP::DEBUG_SERVER;
-// // Set the email recipients, subject, and message
-// $to = "yousafarslan09@gmail.com";
-// $subject = "Test Email";
-// $message = "This is a test email.";
+require './PHPMailer/src/Exception.php';
+require './PHPMailer/src/PHPMailer.php';
+require './PHPMailer/src/SMTP.php';
 
-// // Set the "From" address
-// $mail->setFrom('yousafarslan09@gmail.com', 'Arslan Yousaf');  // Replace with the sender's email address and name
+    $name =$_POST['name'];
+    $email = $_POST['email'];
+    $subject = $_POST['telephone'];
+    $message = $_POST['message'];
 
-// // Add the recipient address
-// $mail->addAddress($to);
-
-// // Set email body
-// $mail->Body = $message;
-
-// // Try to send the email
-// if (!$mail->send()) {
-//     echo "Mailer Error: " . $mail->ErrorInfo;
-// } else {
-//     echo "Message sent!";
-// }
+    $mail = new PHPMailer(true);
+    $mail->isSMTP();
+    $mail->Host = 'smtp.gmail.com';
+    $mail->SMTPAuth = true;
+    $mail->Username = 'yousafarslan09@gmail.com';
+    $mail->Password = 'tzmfmpjqtdhbjzvo';
+    $mail->Port = 465;
+    $mail->SMTPSecure = 'ssl';
+    $mail->isHTML(true);
+    $mail->setFrom('info@arclighttech.net', $name);
+    $mail->addAddress('info@arclighttech.net');
+    $mail->Subject = ("$email ($subject)");
+    $mail->Body = $message;
+    $mail->send();
+    echo "
+    <script>
+    alert('Email Sent Successfully');
+    </script>
+    ";
 ?>
